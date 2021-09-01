4f2ea6df97301536fa9c6fbac961ae12d90f0987
FrancescoCina <francesco.cina04@gmail.com>
2021-09-01 16:12:06 +0200

    generate 10 mail e pushate nell'array con for e stampate in HTML le mail in una lista

--- js/script.js
+++ js/script.js
@@ -5,16 +5,21 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: "#app",
     data: {
-        listOfEmails: [],
+        emails: [],
     },
     methods: {
-
+        mountArrayWithTenEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res) => {
+                    const responseEmail = res.data.response;
+                    this.emails.push(responseEmail);
+                })
+            }
+        }
     },
     created() {
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res) => {
-            const responseEmail = res.data.response;
-            this.listOfEmails.push(responseEmail);
-            console.table(this.listOfEmails);
-        })
+        this.mountArrayWithTenEmails();
+        console.log(this.emails);
+
     },
 })
