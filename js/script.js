a1c892d41b4c2868ae30d0f1f6fca0846a881a9c
FrancescoCina <francesco.cina04@gmail.com>
2021-09-01 16:04:54 +0200

    Creato array di appoggio, presa una mail da Api e pushata nell'array creato

--- js/script.js
+++ js/script.js
@@ -4,7 +4,17 @@ Vue.config.devtools = true;
 
 const app = new Vue({
     el: "#app",
-    data: {},
-    methods: {},
-    created() { },
+    data: {
+        listOfEmails: [],
+    },
+    methods: {
+
+    },
+    created() {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res) => {
+            const responseEmail = res.data.response;
+            this.listOfEmails.push(responseEmail);
+            console.table(this.listOfEmails);
+        })
+    },
 })
